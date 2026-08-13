a23fe57e5a3edd7398d312b5722e570c9ccac55c
Rafou2898 <rafael.dousse@hotmail.fr>
2026-08-13 14:43:07 +0200

    feat: led implementation

--- led/led.c
+++ led/led.c
@@ -8,6 +8,8 @@ static struct gpiod_line_config *line_cfg;
 static struct gpiod_request_config *req_cfg;
 static struct gpiod_line_request *request;
 
+#define CHIP_NAME "/dev/gpiochip0" 
+
 #define LED_RED_PIN   (16)
 #define LED_GREEN_PIN (20)
 #define LED_BLUE_PIN  (21)
@@ -19,8 +21,8 @@ int led_init(void)
 	}
 
 	unsigned int led_pins[] = {LED_RED_PIN, LED_GREEN_PIN, LED_BLUE_PIN};
-
-	chip = gpiod_chip_open("todo");
+	const int num_leds = sizeof(led_pins) / sizeof(led_pins[0]);
+	chip = gpiod_chip_open(CHIP_NAME);
 	if (!chip) {
 		perror("gpiod_chip_open");
 		return 1;
@@ -36,7 +38,7 @@ int led_init(void)
 	gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_INACTIVE);
 
 	line_cfg = gpiod_line_config_new();
-	gpiod_line_config_add_line_settings(line_cfg, led_pins, 1, settings);
+	gpiod_line_config_add_line_settings(line_cfg, led_pins, num_leds, settings);
 
 	req_cfg = gpiod_request_config_new();
 	gpiod_request_config_set_consumer(req_cfg, "led");
@@ -46,10 +48,71 @@ int led_init(void)
 		perror("gpiod_chip_request_lines");
 		return 1;
 	}
+
+	for (int i = 0; i < 3; i++) {
+		led_set((led_id_t)i, false);
+	}
+
 }
-int led_set(led_id_t led, bool state)
-{
+
+int led_set(led_id_t led, bool state){
+	unsigned int pin;
+		switch (led) {
+		case LED_RED:
+			pin = LED_RED_PIN;
+			break;
+		case LED_GREEN:
+			pin = LED_GREEN_PIN;
+			break;
+		case LED_BLUE:
+			pin = LED_BLUE_PIN;
+			break;
+		default:
+		printf("Invalid LED ID: %d\n", led);
+			return -1;
+		}
+		int value = gpiod_line_request_set_value(request, pin, (enum gpiod_line_value)state);
+
+		if(value < 0) {
+			perror("gpiod_line_request_set_value");
+			return -1;
+		}
+
 }
+
+int led_get(led_id_t led, bool *state){
+	unsigned int pin;
+	if (!request || !state) {
+		fprintf(stderr, "LED not initialized or invalid state pointer\n");
+		return -1;
+	}
+
+	switch (led) {
+	case LED_RED:
+		pin = LED_RED_PIN;
+		break;
+	case LED_GREEN:
+		pin = LED_GREEN_PIN;
+		break;
+	case LED_BLUE:
+		pin = LED_BLUE_PIN;
+		break;
+	default:
+		printf("Invalid LED ID: %d\n", led);
+		return -1;
+	}
+
+	int value = gpiod_line_request_get_value(request, pin);
+	if (value < 0) {
+		perror("gpiod_line_request_get_value");
+		return -1;
+	}
+	*state = (value == GPIOD_LINE_VALUE_ACTIVE);
+	return 0;
+}
+
+
+
 int led_deinit(void)
 {
 
--- led/led.h
+++ led/led.h
@@ -1,10 +1,11 @@
 #ifndef LED_H
 #define LED_H
 
-#include "protocol/protocol.h"
+#include "protocol.h"
 
 int led_init(void);
 int led_set(led_id_t led, bool state);
+int led_get(led_id_t led, bool *state);
 int led_deinit(void);
 
 #endif /*LED_H*/
